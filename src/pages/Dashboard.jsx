3f7d8d751b35e57abf85006c6bae2e62c9777ace
vanshgoswami <vanshgoswami93@gmail.com>
2026-02-04 13:42:45 +0530

    [HG] [file are added]

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import TaskList from "../components/TaskList";
 
 const Dashboard = () => {
-  return (
-    <div>hello</div>
+  const navigate = useNavigate() 
+    
+    const[data,setTasks]=useState([])
+
+    useEffect(()=>{
+      fetchData();
+    },[]);
+    
+
+    const fetchData =  async()=>{
+      try{
+        const responce=await fetch
+        const data=responce.json();
+        setTasks(data);
+      }
+        catch(error){
+          console.log(error);
+        
+      }
+    }
+  
+
+  const handleLogout = () => {
+    localStorage.removeItem('loginData')
+    localStorage.removeItem('authData')
+    // localStorage.clear()
+    navigate('/login')
+  }
+  return(
+   <div>
+    <Navbar title="Task Manager" onLogout={handleLogout}/>
+    <h1>My Task</h1>
+    <TaskList/>
+   </div>
   )
 }
 
